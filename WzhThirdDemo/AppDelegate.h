05125258412042370e78adfa4fbbf52b381dc10c
2446886848 <2446886848@qq.com>
2016-04-11 09:21:24 +0800

    Initial Commit

---
+++ WzhThirdDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WzhThirdDemo
+//
+//  Created by 吴志和 on 16/4/11.
+//  Copyright © 2016年 吴志和. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WzhThirdDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WzhThirdDemo
+//
+//  Created by 吴志和 on 16/4/11.
+//  Copyright © 2016年 吴志和. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
